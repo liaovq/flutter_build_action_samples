1dbfe7e9c7ec8932a426c4f057f116adb733b5a8
liaovq <liaovq@gmail.com>
2022-03-09 23:26:37 +0800

    feat: test evn build config

--- android/app/src/main/kotlin/com/example/flutter_build_action_samples/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_build_action_samples/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.flutter_build_action_samples
 
+import android.os.Bundle
+import android.util.Log
+import io.flutter.BuildConfig
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "env is debug: ${BuildConfig.DEBUG}")
+        Log.d("MainActivity", "env is profile: ${BuildConfig.PROFILE}")
+        Log.d("MainActivity", "env is release: ${BuildConfig.RELEASE}")
+    }
 }
